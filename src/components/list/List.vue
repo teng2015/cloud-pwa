845d07f3df516912ec949eded8ff02706bab7898
Simple <simpliberty@gmail.com>
2017-03-21 16:42:33 +0800

    2017-03-21

--- src/components/list/List.vue
+++ src/components/list/List.vue
@@ -21,39 +21,33 @@
     </div>
 
     <ul class="file-lists">
-      <li class="item">
-        <div class="col col-name">
-          <div class="column">
-            <div class="column-name column-folder">
-              <span class="file-icon icon-folder"></span>
-              <span class="text">同步盘</span>
-            </div>
+      <li>
+        <div class="file">
+          <div class="column-folder">
+            <span class="file-icon icon-folder"></span>
+            <span class="text">同步盘</span>
           </div>
         </div>
       </li>
     </ul>
 
-    <!--<div class="col col-name">
-      <div class="column">
-        <div class="column-name column-file">
-          <a href="javascript:">
-            <span class="icon icon-file-type icon-pdf"></span>
-          </a>
-          <div class="column-file-info">
-            <div class="text">欢迎使用天翼云盘.pdf</div>
-            <div class="size">大小：2.24M</div>
-            <div class="modified-time">21小时前</div>
-          </div>
-          <div class="file-info" style="display: none;">
-            <div class="btn">
-              <a href="javascript:" class="preview">预览</a>
-              <a href="javascript:" class="share">分享</a>
-            </div>
-          </div>
-          <div class="arrow"></div>
+    <div class="column">
+      <div class="column-file">
+        <span class="icon icon-file-type icon-pdf"></span>
+        <div class="column-file-info">
+          <div class="text">欢迎使用天翼云盘.pdf</div>
+          <div class="size">大小：2.24M</div>
+          <div class="modified-time">21小时前</div>
         </div>
+        <div class="file-info" style="display: none;">
+          <div class="btn">
+            <a href="javascript:" class="preview">预览</a>
+            <a href="javascript:" class="share">分享</a>
+          </div>
+        </div>
+        <div class="arrow"></div>
       </div>
-    </div>-->
+    </div>
   </div>
 </template>
 
@@ -62,7 +56,7 @@
     props: ['list'],
     data () {
       return {
-        msg: 'msg'
+        isFolder: true
       }
     }
   }
